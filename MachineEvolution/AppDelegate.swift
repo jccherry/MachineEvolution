754b41a705866f056ab69e83044fe4edf47b4208
jccherry <jchiaramonte18@regis.org>
2016-09-27 11:50:12 -0400

    initial commit

---
+++ MachineEvolution/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MachineEvolution
+//
+//  Created by John Chiaramonte on 9/27/16.
+//  Copyright © 2016 John Chiaramonte. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MachineEvolution/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MachineEvolution
+//
+//  Created by John Chiaramonte on 9/27/16.
+//  Copyright © 2016 John Chiaramonte. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
